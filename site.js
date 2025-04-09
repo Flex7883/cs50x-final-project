8e19d31b9cc6601beca07881ccc0bc9dc1dc458b
Flex7883 <h2co31004+github@gmail.com>
2025-04-10 06:22:43 +0800

    Reconstruct site.js to use PTT's site built-in ima ge preview structure
    - Create container for image
    - Let site handle container and image CSS
    - Insert container after links

--- site.js
+++ site.js
@@ -1,24 +1,40 @@
 // Find all links in the page
 const links = document.querySelectorAll('a');
 
+// Set image container
+let previewElement = null;
+
 // Iterate through all links and append the image preview element
 links.forEach(link => {
     let imageUrl = link.href;
 
     // Check if the link is an image URL
     if (isImgurUrl(imageUrl)) {
+
+        // Resolve imgur links format
         imageUrl = resolveImgurUrl(imageUrl);
+
+        // Create image container
+        previewElement = document.createElement('div');
+        previewElement.classList.add('richcontent');
+
+        // Create image and set attributes
         const img = document.createElement('img');
         img.src = imageUrl;
-        img.classList.add('image-preview-thumbnail');
+        img.loading = 'lazy';
         img.referrerPolicy = 'no-referrer';
 
-        // Image fails loading handling
+        // Skipping preview if image failed to load
         img.onerror = function() {
             this.style.display = 'none';
             console.log('Failed to load image: ', img.src);
+            return;
         }
 
-        link.insertAdjacentElement('afterend', img);
+        // Append the image to the preview container
+        previewElement.appendChild(img);
+        
+        // Insert the preview element after the link
+        link.insertAdjacentElement('afterend', previewElement);
     }
 });
